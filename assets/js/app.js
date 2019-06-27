a0047ca8b4ee233d0a5f331e60fd0ed05a5d6196
Tth <mtoutouhi@gmail.com>
2019-06-27 09:29:09 +0200

    page message finie (en attendant le fond d'ecran de la page)

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/app.scss');
-require('../css/message.css');
+require('../scss/message.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
