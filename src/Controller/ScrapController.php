670265e8d9c2f91d6261b2e4d06eae58d0401ddc
giroz <melanie>
2019-06-27 17:24:09 +0200

    try

--- src/Controller/ScrapController.php
+++ src/Controller/ScrapController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\DomCrawler\Crawler;
 use Goutte\Client;
 use Symfony\Component\BrowserKit\AbstractBrowser;
+use Symfony\Component\DomCrawler\Crawler;
 
 
 
@@ -21,7 +21,7 @@ class ScrapController extends AbstractController
         $crawler = $client->request('GET', 'https://www.just-eat.fr/restaurant-livraison-a-domicile/restaurant/oky-sushi-75020');
 
         $menus = [];
-        $crawler->filter('li')->filter('.product')->each(function ($node){
+        $crawler->filter('li')->filter('.product')->each(function ($node) use (&$menus){
             
              $menu['name'] = trim($node->filter('h4')->filter('.name')->text());
              if(!empty($node->filter('div')->filter('.description')))
@@ -41,34 +41,36 @@ class ScrapController extends AbstractController
         }); 
         dump($menus);
 
-        // $images = $crawler->filter('img')->each(function ($node) {
-        //     echo '<img src="' . $node->attr('src') . '" alt="' . $node->attr('itemprop') . '">';
-        // });
-        // // image
-
-        // $plat = $crawler->filter('h4')->filter('.name')->each(function ($node){
-        //     return $posts[] = $node->text();
-        // }); 
-
-        // // plat
-
-        // $description = $crawler->filter('div')->filter('.description')->each(function ($node){
-        //     return $posts[] = $node->text();
-        // }); 
-
-        // // description
-
-        // $prix = $crawler->filter('div')->filter('.price')->each(function ($node){
-        //     return $posts[] = $node->text();
-        // }); 
-
-        //  // prix
+        
         
         $tag = $crawler->filter('div')->filter('.cuisines')->filter('a')->each(function ($node){
             return $posts[] = $node->text();
         }); 
 
-        // // tag
+        // tag
+
+        // $images = $crawler->filter('img')->each(function ($node) {
+        //     echo '<img src="' . $node->attr('src') . '" alt="' . $node->attr('itemprop') . '">';
+        // });
+         // image
+
+        // $plat = $crawler->filter('h4')->filter('.name')->each(function ($node){
+        //     return $posts[] = $node->text();
+        // }); 
+
+         // plat
+
+        // $description = $crawler->filter('div')->filter('.description')->each(function ($node){
+        //     return $posts[] = $node->text();
+        // }); 
+
+        // description
+
+        // $prix = $crawler->filter('div')->filter('.price')->each(function ($node){
+        //     return $posts[] = $node->text();
+        // }); 
+
+        // prix
        
         return $this->render('scrap/index.html.twig', [
             'controller_name' => 'ScrapController',
