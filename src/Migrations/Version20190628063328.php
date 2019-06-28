3e684be198c23deb7e6a2d0ff28c832d36f06245
Adeline Jacquot <ade.jacquot@gmail.com>
2019-06-28 08:35:03 +0200

    modification bouton de sélection

--- src/Migrations/Version20190628063328.php
+++ src/Migrations/Version20190628063328.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190627100747 extends AbstractMigration
+final class Version20190628063328 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20190627100747 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE plats (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, image VARCHAR(255) NOT NULL, tag VARCHAR(100) NOT NULL, restaurant VARCHAR(100) NOT NULL, prix DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE plats DROP restaurant');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20190627100747 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE plats');
+        $this->addSql('ALTER TABLE plats ADD restaurant VARCHAR(100) NOT NULL COLLATE utf8mb4_unicode_ci');
     }
 }
